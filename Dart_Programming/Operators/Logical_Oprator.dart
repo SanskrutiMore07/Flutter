2ff37b96a576200f58c946d1af90930c4ce6c98e
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-03-07 13:27:21 +0530

    Program

---
+++ Dart_Programming/Operators/Logical_Oprator.dart
@@ -0,0 +1,30 @@
+/*void main()
+{
+  //Logical AND &&
+  var A= 10;
+  var B = 20;
+  if(A < 15 && B >15)
+  {
+    print(true);
+  }
+  else{
+    print(false);
+  }
+}*/
+
+
+//Logical OR ||
+
+void main()
+{
+  //Logical OR ||
+  var A= 10;
+  var B = 20;
+  if(A < 15 || B < 15)
+  {
+    print(true);
+  }
+  else{
+    print(false);
+  }
+}
