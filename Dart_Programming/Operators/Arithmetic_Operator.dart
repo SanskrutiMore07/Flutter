63a0c95e9d2c04296ccd8bd1b517943912718c17
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-03-07 13:21:58 +0530

    Program

---
+++ Dart_Programming/Operators/Arithmetic_Operator.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  int A = 10;
+  int B = 20;
+
+  var Sum = A+B;
+  print("Sum Of Given Number Is : $Sum");
+  var Sub = A-B;
+  print("Sub Of Given Number Is : $Sub");
+  var Mult = A*B;
+  print("Multiplication Of Given Number Is : $Mult");
+  var Div = A/B;
+  print("Division Of Given Number Is : $Div");
+  var Modulo = A+B;
+  print("Modulo Of Given Number Is : $Modulo");
+}
