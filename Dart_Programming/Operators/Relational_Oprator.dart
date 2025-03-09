3892af45f0aa8d44a048d20f7b8644dd2bd9f862
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-03-09 23:52:32 +0530

    Program

---
+++ Dart_Programming/Operators/Relational_Oprator.dart
@@ -0,0 +1,41 @@
+/*void main()
+{
+  int A = 50;
+  int B = 70;
+
+  if(A>B)
+  {
+    print("A Is Grater than B");
+  }
+  else{
+    print("B Is Grater than A");
+  }
+}*/
+
+/*void main()
+{
+  int A = 80;
+  int B = 90;
+
+  if(A>=B)
+  {
+    print("A Is Grater Than Equal To B");
+  }
+  else{
+    print("B Is Grater Than Equal A");
+  }
+}*/
+
+void main()
+{
+  int A = 80;
+  int B = 80;
+
+  if(A==B)
+  {
+    print("A Is Equal To B");
+  }
+  else{
+    print("A Is Not Equal B");
+  }
+}
