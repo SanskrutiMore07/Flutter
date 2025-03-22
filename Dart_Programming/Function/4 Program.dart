890f16244dea107ad3674ab1d6470ea9e3a12070
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-03-22 15:13:35 +0530

    Program

---
+++ Dart_Programming/Function/4 Program.dart
@@ -0,0 +1,11 @@
+//No Parameters,No Return Type
+
+void sum(){                      ///User Define Fun
+  int  No1 = 5, No2 =10;         ///Variable Declaration And Initialization
+  int? res;                      ///Declaration
+  res = No1+No2;                 ///Opration 
+  print("Addition Of Two Numbers : ${res}"); ///print
+}
+void main(){                     ///Entry Point Fun
+  sum();                        ///Function Call
+}
