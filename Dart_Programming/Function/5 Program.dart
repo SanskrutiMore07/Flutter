e7ec2cd9280d825f180fd2ffcafb3267750b636a
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-03-22 15:14:06 +0530

    Program

---
+++ Dart_Programming/Function/5 Program.dart
@@ -0,0 +1,11 @@
+// Parameters with Return Type
+
+
+int? sum(int  No1 , int No2 ){        ///User Define Fun
+  return (No1+No2) ;               ///Opration 
+ 
+}
+int? main(){                     ///Entry Point Fun
+  int? No = sum(10,20);          ///Function Call
+  print(No);                      ///print
+}
