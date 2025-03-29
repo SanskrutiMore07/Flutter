c58a211c014ec30485b8018d8dc5a7a987d048e9
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-03-29 18:54:59 +0530

    Program

---
+++ Dart_Programming/Data_Types/Program 6.dart
@@ -0,0 +1,7 @@
+//Boolean
+void main(){              //Entry Point Function
+  bool number = true;        // Declaration
+  print(number);
+  print(number.runtimeType); //int
+  print(number.hashCode);     // Vaitual Address
+}
