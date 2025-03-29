f45dee34f1d2c124805928e7cf4f77a4b6fac463
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-03-29 18:54:41 +0530

    Program

---
+++ Dart_Programming/Data_Types/Program 5.dart
@@ -0,0 +1,7 @@
+//Number
+void main(){              //Entry Point Function
+  num number = 10;        // Declaration
+  print(number);
+  print(number.runtimeType); //int
+  print(number.hashCode);     // Vaitual Address
+}
