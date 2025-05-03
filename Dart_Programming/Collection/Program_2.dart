a9ca2997e4213f9a00eae543c937acc0a1bdd174
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-05-03 22:00:27 +0530

    Program

---
+++ Dart_Programming/Collection/Program_2.dart
@@ -0,0 +1,9 @@
+void main(){
+  List namelist =['Sahil','Rohan','Raj','Tanmay'];
+  print(namelist);
+  namelist.add('Sakshi');
+  print(namelist);
+  namelist.remove('Sahil');
+  print(namelist);
+  
+}
