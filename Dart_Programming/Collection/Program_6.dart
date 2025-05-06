dfbfd9e33632fe7335005d78b83e0bd43f9f32a8
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-05-06 20:44:25 +0530

    Collection

---
+++ Dart_Programming/Collection/Program_6.dart
@@ -0,0 +1,14 @@
+//future <string>,awa
+
+void main(){
+  printData();
+}
+void printData(){
+ print('My College Name :');
+ getData();
+ print('YC College Karad ');
+ print('Computer Department!!!');
+}
+Future<String> getData()async{
+return 'ForkInfosystem';
+}
